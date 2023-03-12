e532bc42a5d867f66893d98938592c84b4bcb7eb
bogdanovamaria <bogdanova.maria@abv.bg>
2023-03-12 13:38:08 +0000

    adding jenkinsfile and code

---
+++ infrastructure/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  name          = "flask-final"
+  vpc_id        = "vpc-087c16a8b741e7b15"
+  subnet_id     = "subnet-017ca7237ba003a6a"
+  instance_type = "t2.micro"
+  my_ip         = "0.0.0.0/0"
+}
